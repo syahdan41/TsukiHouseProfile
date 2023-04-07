757dea95f01e5458302f3b21a23621a09b396b52
syahdan41 <syahdantriantoro41@gmail.com>
2023-04-08 00:08:45 +0700

    FoundingCardComponent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import FoundingCard from "./Component/FoundingCardComponent/FoundingCard";
+import React from "react";
+import profile1 from "./Assets/image/Chaewon (6).png";
+import profile2 from "./Assets/image/syahdansayangseulgi.png";
+import profile3 from "./Assets/image/gantengnyajaran.png";
+import profile4 from "./Assets/image/hanif.png";
+import profile5 from "./Assets/image/ikramhentai.png";
+import igIcon from "./Assets/image/iconig.png";
+import linkedinPict from "./Assets/image/linkedin.png";
 function App() {
+  const FounderData = [
+    {
+      name: "Putra Bangsa W.",
+      role: "Founding Member",
+      image: profile1,
+      linkedin: linkedinPict,
+      linkLinkedin: "https://www.linkedin.com/in/putra-bangsa-widjanarko-0890b2223/",
+      instagram: igIcon,
+      linkInstagram: "https://www.instagram.com/sapikayang/",
+    },
+    {
+      name: "Syahdan Triantoro",
+      role: "Founding Member",
+      image: profile2,
+      linkedin: linkedinPict,
+      linkLinkedin: "https://www.linkedin.com/in/syahdan-triantoro/",
+      instagram: igIcon,
+      linkInstagram: "https://www.instagram.com/syhdn_t/",
+    },
+    {
+      name: "Pandu Wirakusuma",
+      role: "President",
+      image: profile3,
+      linkedin: linkedinPict,
+      linkLinkedin: "https://www.linkedin.com/in/muhammad-pandu-549977207/",
+      instagram: igIcon,
+      linkInstagram: "https://www.instagram.com/pandu_wirakusuma/",
+    },
+    {
+      name: "Hanif Faiq",
+      role: "Founding Member",
+      image: profile4,
+      linkedin: linkedinPict,
+      linkLinkedin: "https://www.linkedin.com/in/haniffaiq13/",
+      instagram: igIcon,
+      linkInstagram: "https://www.instagram.com/lama2dingin/",
+    },
+    {
+      name: "Ikram Andika Ukar",
+      role: "Founding Member",
+      image: profile5,
+      linkedin: linkedinPict,
+      linkLinkedin: "https://www.linkedin.com/in/ikramau/",
+      instagram: igIcon,
+      linkInstagram: "https://www.instagram.com/ikramau/",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <div className="headingFounding">
+        <p className="foundingTittle">Our Beloved Founding Members</p>
+        <p className="foundingDesc">
+          Our beloved founding members were the visionaries who paved the way for the establishment of our organization. They were driven by a passion to create something meaningful and impactful that would benefit society in a significant
+          way. Their unwavering commitment, tireless efforts, and countless sacrifices laid the foundation for our success today.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <FoundingCard CardData={FounderData} />
     </div>
   );
 }
---
+++ src/Component/FoundingCardComponent/FoundingCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./FoundingCard.css";
+function FoundingCard({ CardData }) {
+  const CardDataList = CardData.map((item, index) => (
+    <div key={index}>
+      <div className="profileCard">
+        <img src={item.image} alt={item.title} className="profileImage" />
+      </div>
+      <div className="card">
+        <p className="nameStyle">{item.name}</p>
+        <p className="roleStyle">{item.role}</p>
+        <div className="socmedContainer">
+          <a href={item.linkInstagram} target="_blank" key={item.linkInstagram}>
+            <img src={item.instagram} alt="linkedin" className="Icon" />
+          </a>
+          <a href={item.linkLinkedin} target="_blank" key={item.linkLinkedin}>
+            <img src={item.linkedin} alt="linkedin" className="Icon" />
+          </a>
+        </div>
+      </div>
+    </div>
+  ));
+  return (
+    <section>
+      <div className="FoundingCardLayout">{CardDataList}</div>
+    </section>
+  );
+}
+export default FoundingCard;
